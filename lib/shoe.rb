2056d75f823416c0e716abc415b5d4bb6eb835ac
Sara Kantor <sara.kantor.nv@gmail.com>
2018-10-09 17:02:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe 
   
-  attr_accessor :color, :material, :condition
+  attr_accessor :color, :material, :condition, :size
   attr_reader :brand 
   
   def initialize(brand) 
